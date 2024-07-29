8ce026870bcd6acc45077dd0df078999efbe690c
Argya Vityasy <149230734+vityasyyy@users.noreply.github.com>
2024-07-30 00:32:12 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
   useEffect(() => {
     const checkUserLoggedIn = async () => {
       try {
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/auth/validate`, { withCredentials: true });
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/auth/validate`, { withCredentials: true, headers: {'Content-Type': 'application/json'} });
         console.log(response);
         if (response.data.user) {
           setUser(response.data.user);
